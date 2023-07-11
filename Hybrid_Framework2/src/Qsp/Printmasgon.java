67d1a2f0bfe8357b7ff6f6ed31ce966faeb01795
HP <HP@DESKTOP-LD2GPJV>
2023-07-11 15:25:54 +0530

    1st Commit

---
+++ Hybrid_Framework2/src/Qsp/Printmasgon.java
@@ -0,0 +1,30 @@
+package Qsp;
+
+import java.io.IOException;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.PatternLayout;
+import org.testng.annotations.Test;
+
+import freemarker.log.Logger;
+
+public class Printmasgon 
+{
+	@Test
+	public void test1() throws IOException
+	{
+		Layout lay = new PatternLayout("%d %c %m %n");
+		Appender app = new FileAppender(lay,"./Report1/r6.log");
+		
+		BasicConfigurator.configure();
+		
+	 Logger log = Logger.getLogger(this.getClass().getName());
+	 log.warn("warn");
+	 log.info("info");
+	 log.error("error");
+	}
+
+}
